64f9ad6a58bdec9b85ce1b5d7243107a4bfce47d
Alexander <alex17.05.84@yandex.ru>
2022-10-24 16:21:01 +0300

    Decision Example 01

--- Program.cs
+++ Program.cs
@@ -1,5 +1,29 @@
-﻿
+﻿// void Insert(int a)
+// {
+//    Console.WriteLine("Вставить + a");
+// } 
 
+int CreateNumber()
+{
+    int number = new Random().Next(10,100);
+    return number;
+}
 
+int MaxDigit(int number)
+{
+    int Decimal = number/10;
+    int Ed = number % 10;
 
-
+    if (Decimal >= Ed)
+    {
+        return Decimal;
+    }
+    else
+    {
+    return Ed;
+    }
+}
+int a = CreateNumber();
+Console.WriteLine(a);
+int maxDigit = MaxDigit(a);
+Console.WriteLine(maxDigit);
